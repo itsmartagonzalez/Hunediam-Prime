520588e2cf26133dd3d142f450c93cd21c7f0943
Thaddaus Haase <alu0101016733@ull.edu.es>
2021-11-23 14:59:20 +0000

    added examples

---
+++ selectFromDatabase.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3.6
+
+# SQLlite3 : https://docs.python.org/3/library/sqlite3.html
+
+import sqlite3
+import re
+
+# Files
+
+database = "database/test.db"
+
+# connection to database
+
+databaseConnection = sqlite3.connect(database)
+
+dbSql = databaseConnection.cursor();
+
+# SELECT titles from movies
+
+# movieTitle = dbSql.execute('''SELECT movie.title FROM movie''').fetchall()
+# print(movieTitle)
+# print("Found " + str(len(movieTitle)) + " Results")
+
+# SELECT titles of movies from genre ...
+
+# genre = "Comedy"
+#
+# movieTitle = dbSql.execute('''
+#     SELECT movie.title FROM movie INNER JOIN movieGenres INNER JOIN genre
+#         on movie.id = movieGenres.id_movie
+#         and movieGenres.id_genre = genre.id
+#         and genre.genre = ?
+# ''', (genre,)).fetchall()
+# print(movieTitle)
+# print("Found " + str(len(movieTitle)) + " Results")
+
+# SELECT id and titles of movies from genre ... that have at least a rating of ...
+
+genre = "Crime"
+rating = 5.0
+
+movieTitle = dbSql.execute('''
+    SELECT movie.id, movie.title FROM movie INNER JOIN movieGenres INNER JOIN genre INNER JOIN rating
+        on movie.id = movieGenres.id_movie
+        and movieGenres.id_genre = genre.id
+        and movie.id = rating.id_movie
+        and genre.genre = ?
+        and rating.rating >= ?
+''', (genre, rating,)).fetchall()
+print(movieTitle)
+print("Found " + str(len(movieTitle)) + " Results")
+
+# closing conection
+databaseConnection.close()
