90bb7d686940694f56cd3ea060386576691fd885
Thaddaus Haase <alu0101016733@ull.edu.es>
2021-12-18 17:56:19 +0000

    comparison between count vectorizer and tfidf

--- contentBasedTesting.py
+++ contentBasedTesting.py
@@ -3,11 +3,11 @@
 #Applied: https://medium.com/analytics-vidhya/content-based-recommender-systems-in-python-2b330e01eb80
 
 import numpy as np
-import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 import sqlite3
 import re
@@ -22,17 +22,8 @@ movies = dbSql.execute('''SELECT movie.id, movie.title, movie.overview FROM movi
 movies = np.array(movies)
 # print(movies[:,2])
 
-#creating ...
-tfidf = TfidfVectorizer(stop_words='english')
-#Construct the required TF-IDF matrix by applying the fit_transform method on the overview field of the movies
-overview_matrix = tfidf.fit_transform(movies[:,2].tolist())
-#Output the shape of tfidf_matrix
-# print(f"{overview_matrix.shape}")
 
-similarity_matrix = linear_kernel(overview_matrix,overview_matrix)
-# print(similarity_matrix)
-
-def recommend_movies_based_on_plot(movieID):
+def getSimilarity(similarity_matrix, movieID):
     #get similarity values with other movies
     #similarity_score is the list of index and similarity matrix
     similarity_score = list(enumerate(similarity_matrix[movieID]))
@@ -44,14 +35,37 @@ def recommend_movies_based_on_plot(movieID):
     similarMovies = [i[0] for i in similarity_score]
     return similarMovies
 
-    # movie_indices = [i[0] for i in similarity_score]
-    # return (movies[‘title’].iloc[movie_indices])
+def tfidfTesting(movieID, movies=movies):
+    #creating ...
+    tfidf = TfidfVectorizer(stop_words='english')
+    #Construct the required TF-IDF matrix by applying the fit_transform method on the overview field of the movies
+    overview_matrix = tfidf.fit_transform(movies[:,2].tolist())
+    #Output the shape of tfidf_matrix
+    # print(f"{overview_matrix.shape}")
+    similarity_matrix = linear_kernel(overview_matrix,overview_matrix)
+    # print(similarity_matrix)
+    return getSimilarity(similarity_matrix, movieID)
+
+
+#Count vectorizer testing
+def cvTesting(movieID, movies=movies):
+    cv = CountVectorizer(stop_words='english');
+    overview_matrix = cv.fit_transform(movies[:,2].tolist())
+    cosine_sim = cosine_similarity(overview_matrix, overview_matrix)
+    return getSimilarity(cosine_sim, movieID)
+
 
 print(movies[:,1][0:20])
 # passing movie id (index) into function to get similar movies
 forId = 1
-similarMovies = recommend_movies_based_on_plot(forId)[1:10]
-
+print()
+similarMovies = tfidfTesting(forId)[1:10]
 print("Similar movies for: " + movies[forId][1])
 for id in similarMovies:
-    print(movies[id])
+    print(movies[id][1])
+
+print()
+similarMovies = cvTesting(forId)[1:10]
+print("Similar movies for: " + movies[forId][1])
+for id in similarMovies:
+    print(movies[id][1])
---
+++ surpriseLibraryTesting.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.6
+
+# https://realpython.com/build-recommendation-engine-collaborative-filtering/
+
+# sudo pip3 install scikit-surprise
+# if failing with python setup.py...
+# sudo apt-get install python3-setuptools
+# if failing due to Python.h not found:
+# sudo apt-get install python3-dev
