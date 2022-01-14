d5c81c2cbf540eade94be7f550f1692950f9f973
Thaddaus Haase <alu0101016733@ull.edu.es>
2022-01-14 20:06:18 +0000

    run python from index.js working

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-const {PythonShell} = require('python-shell')
-const path = require("path")
-
-
 const runPython = (program, arguments, callback) => {
 
-  let options = {
-    scriptPath : path.join(__dirname, '/'),
-    args : arguments
-  }
-
-  let pyshell = new PythonShell(program, options);
-
-
-  pyshell.on('message', function(message) {
-    callback(message);
+  let programToRun = [program, ...arguments];
+  const python = require('child_process').spawn('python', programToRun);
+  python.stdout.on('data',function(data){
+    //console.log("data: ",data.toString('utf8'));
+    callback(data.toString('utf8'));
   });
-  // document.getElementById("city").value = "";
-  // let programToRun = [program, ...arguments];
-  // const python = require('child_process').spawn('python', programToRun);
-  // python.stdout.on('data',function(data){
-  //   //console.log("data: ",data.toString('utf8'));
-  //   callback(data.toString('utf8'));
-  // });
 }
 
 const infoPrint = (info) => {
