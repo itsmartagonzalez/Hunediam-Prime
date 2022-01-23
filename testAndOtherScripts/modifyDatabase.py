38a29eda0aef3ae377992c2663efcc999a616bf4
Thaddaus Haase <alu0101016733@ull.edu.es>
2022-01-23 09:57:18 +0000

    added svdTrainblock first entry

--- testAndOtherScripts/modifyDatabase.py
+++ testAndOtherScripts/modifyDatabase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sqlite3
+import datetime
 
 # Files
 
@@ -13,30 +14,9 @@ databaseConnection = sqlite3.connect(database)
 
 dbSql = databaseConnection.cursor();
 
-dbSql.execute('''CREATE TABLE svdTrainBlock(
-    id INTEGER PRIMARY KEY,
-    test_date DATE NOT NULL,
-    min_ratings INTEGER NOT NULL,
-    description TEXT DEFAULT NULL)''')
-dbSql.execute('''CREATE TABLE svdStatistics(
-    id INTEGER PRIMARY KEY,
-    id_block INTEGER NOT NULL,
-    n_epochs INTEGER NOT NULL,
-    lr_all FLOAT NOT NULL,
-    reg_all FLOAT NOT NULL,
-    rmse FLOAT NOT NULL,
-    mae FLOAT NOT NULL,
-    right_on FLOAT DEFAULT NULL,
-    still_good FLOAT DEFAULT NULL,
-    meh FLOAT DEFAULT NULL,
-    bad FLOAT DEFAULT NULL,
-    FOREIGN KEY(id_block) REFERENCES svdTrainBlock(id))''')
-dbSql.execute('''CREATE TABLE userStatistics(
-    id INTEGER PRIMARY KEY,
-    id_user INTEGER NOT NULL,
-    id_block INTEGER NOT NULL,
-    FOREIGN KEY(id_block) REFERENCES svdTrainBlock(id),
-    FOREIGN KEY(id_user) REFERENCES user(id))''')
+
+curDate = datetime.datetime.now()
+dbSql.execute("INSERT INTO svdTrainBlock(id, test_date, min_ratings, description) VALUES(?,?,?,?)", (0, curDate, 1, 'Testing insert'))
 
 databaseConnection.commit()
 databaseConnection.close()
