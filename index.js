98c146c4bbdd68d7cda0448fd04512eb32aa28ac
itsmartagonzalez <alu0101254487@ull.edu.es>
2022-01-28 23:39:50 +0000

    Login Screen

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const {remote} = require('electron')
+const ipcRenderer = require('electron').ipcRenderer
+
 const runPython = (program, arguments, callback) => {
   let programToRun = [program, ...arguments];
   const python = require('child_process').spawn('python', programToRun);
@@ -9,7 +12,18 @@ const runPython = (program, arguments, callback) => {
 
 const infoPrint = (info) => {
   console.log(info);
-  alert(info);
+  //alert(info);
 }
 
-runPython('./websitePythonScripts/getMovieData.py', ['1', '2'], infoPrint);
+const textInput = document.getElementById('user-id-input');
+const submitButton = document.querySelector('.submit')
+
+const getTextInput = () => {
+  console.log(textInput.value);
+  // FALTA COMPROBAR EL ID y que no este vacío
+  ipcRenderer.send('change-home');
+}
+
+submitButton.addEventListener('click', () => getTextInput())
+
+//runPython('./websitePythonScripts/getMovieData.py', ['1', '2'], infoPrint);
--- main.js
+++ main.js
@@ -12,12 +12,15 @@
 // }
 
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
 const path = require('path')
+const url = require('url')
+
+let mainWindow = {}
 
 function createWindow () {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -26,7 +29,7 @@ function createWindow () {
       contextIsolation: false
     }
   })
-
+  mainWindow.maximize()
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
@@ -35,6 +38,14 @@ function createWindow () {
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 }
+
+ipcMain.on('change-home', ()=>{
+  BrowserWindow.getAllWindows()[0].loadURL(url.format({
+      pathname : path.join(__dirname,'/pages/homeScreen.html'),
+      protocol:'file',
+      slashes:true
+  }));
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -58,3 +69,6 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+exports.mainWindow = mainWindow;
+exports.app = app;
